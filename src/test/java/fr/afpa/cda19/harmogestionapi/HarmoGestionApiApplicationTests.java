10d81d8199a2e0bc405c64c63970e8959f91ab5e
Thomas <thomasseiwert@hotmail.fr>
2026-04-20 13:07:52 +0200

    TEST: rajout du contextload()

--- src/test/java/fr/afpa/cda19/harmogestionapi/HarmoGestionApiApplicationTests.java
+++ src/test/java/fr/afpa/cda19/harmogestionapi/HarmoGestionApiApplicationTests.java
@@ -1,10 +1,8 @@
 package fr.afpa.cda19.harmogestionapi;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class HarmoGestionApiApplicationTests {
 
