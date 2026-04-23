e9a4f20acf213645ca2a1b13b37029507a024c83
Thomas <thomasseiwert@hotmail.fr>
2026-04-23 09:04:25 +0200

    TESTS: ajout annotations allure-report

--- src/test/java/fr/afpa/cda19/harmogestionapi/controllers/CoursControllerIntegrationTest.java
+++ src/test/java/fr/afpa/cda19/harmogestionapi/controllers/CoursControllerIntegrationTest.java
@@ -2,7 +2,7 @@ package fr.afpa.cda19.harmogestionapi.controllers;
 
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import jdk.jfr.Description;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
