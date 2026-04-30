e11c2a51706453623170a48980dbf815dff25848
Thomas <thomasseiwert@hotmail.fr>
2026-04-30 14:14:08 +0200

    TESTS: ajout tests controllers cours

--- src/test/java/fr/afpa/cda19/harmogestionapi/controllers/CoursControllerIntegrationTest.java
+++ src/test/java/fr/afpa/cda19/harmogestionapi/controllers/CoursControllerIntegrationTest.java
@@ -1,13 +1,22 @@
 package fr.afpa.cda19.harmogestionapi.controllers;
 
+import fr.afpa.cda19.harmogestionapi.models.Cours;
+import fr.afpa.cda19.harmogestionapi.models.Instrument;
+import fr.afpa.cda19.harmogestionapi.models.Membre;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Description;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,13 +37,44 @@ class CoursControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private static final Cours COURS = new Cours();
+
+    @BeforeAll
+    static void initCours() {
+
+        COURS.setDateCours(LocalDateTime.now().plusDays(1));
+        COURS.setDureeCours((byte) 60);
+        COURS.setEnseignant(new Membre(1, "Hendrix", "Jimmi",
+                                       LocalDate.now().minusDays(1),
+                                       null, null));
+        COURS.setInstrument(new Instrument(1, "guitare"));
+        COURS.setParticipants(new ArrayList<>());
+        COURS.getParticipants().add(new Membre(2, "Seiwert", "Thomas",
+                                               LocalDate.now().minusDays(1),
+                                               null, null));
+    }
+
     @Test
-    @Description("Test d'intégration du controller pour récupérer les prochains cours")
+    @DisplayName("Test d'intégration du controller pour retourner la liste des cours")
+    @Description("Envoi d'une requête pour retourner la liste des cours."
+                 + " On s'attend à un statut 200 et à ce que le body contienne le cours.")
     @Severity(SeverityLevel.CRITICAL)
     void getProchainsCoursTest() throws Exception {
 
         mockMvc.perform(get("/cours"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].dureeCours", is(45)));
+    }
+
+    @Test
+    @DisplayName("Test d'intégration du controller pour retourner le cours avec l'id 1")
+    @Description("Envoi d'une requête pour retourner le cours avec l'id 1."
+                 + " On s'attend à un statut 200 et à ce que le body contienne le cours.")
+    @Severity(SeverityLevel.CRITICAL)
+    void getCoursTest() throws Exception {
+
+        mockMvc.perform(get("/cours/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.dureeCours", is(45)));
     }
 }
--- src/test/java/fr/afpa/cda19/harmogestionapi/controllers/CoursControllerTest.java
+++ src/test/java/fr/afpa/cda19/harmogestionapi/controllers/CoursControllerTest.java
@@ -7,6 +7,8 @@ import fr.afpa.cda19.harmogestionapi.services.CoursService;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Description;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -38,21 +40,30 @@ class CoursControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private final Cours cours =
-            new Cours(null, LocalDateTime.now().plusDays(1), (byte) 45,
-                      new Membre(1, "Jimi","Hendrix",
-                              LocalDate.now().minusDays(1),
-                              null, null),
-                      new Instrument(1, "guitare"), new ArrayList<>());
-
+    private static final Cours COURS = new Cours();
 
     @MockitoBean
     private CoursService coursService;
 
+    @BeforeAll
+    static void initCours() {
+
+        COURS.setDateCours(LocalDateTime.now().plusDays(1));
+        COURS.setDureeCours((byte) 60);
+        COURS.setEnseignant(new Membre(1, "Hendrix", "Jimmi",
+                                       LocalDate.now().minusDays(1),
+                                       null, null));
+        COURS.setInstrument(new Instrument(1, "guitare"));
+        COURS.setParticipants(new ArrayList<>());
+        COURS.getParticipants().add(new Membre(2, "Seiwert", "Thomas",
+                                               LocalDate.now().minusDays(1),
+                                               null, null));
+    }
 
     @Test
-    @Description("Test unitaire du controller pour vérifier le statut de"
-                 + " la requête de récupération de la liste des cours")
+    @DisplayName("Test unitaire controller pour retourner la liste des cours")
+    @Description("Envoi d'une requête pour retourner la liste des cours."
+                 + " On s'attend à un statut 404 car BDD vide.")
     @Severity(SeverityLevel.CRITICAL)
     void getAllCoursTest() throws Exception {
 
@@ -60,8 +71,9 @@ class CoursControllerTest {
     }
 
     @Test
-    @Description("Test unitaire du controller pour vérifier le statut de"
-                 + " la requête de récupération d'un cours")
+    @DisplayName("Test unitaire controller pour retourner un cours")
+    @Description("Envoi d'une requête pour retourner un cours avec un identifiant 1."
+                 + " On s'attend à un statut 400 car BDD vide.")
     @Severity(SeverityLevel.NORMAL)
     void getCoursTest() throws Exception {
 
@@ -69,15 +81,14 @@ class CoursControllerTest {
     }
 
     @Test
-    @Description("Test unitaire du controller pour vérifier le statut de"
-                 + " la requête de création d'un cours valide")
+    @DisplayName("Test unitaire controller pour créer un cours avec un identifiant nul")
+    @Description("Envoi d'une requête pour créer un cours avec un identifiant nul."
+                 + " On s'attend à un statut 201.")
     @Severity(SeverityLevel.CRITICAL)
     void createCoursTestOk() throws Exception {
 
-        cours.getParticipants().add(new Membre(2, "Seiwert",
-                "Thomas", LocalDate.now().minusDays(1),
-                null, null));
-        final String json = new ObjectMapper().writeValueAsString(cours);
+        COURS.setIdCours(null);
+        final String json = new ObjectMapper().writeValueAsString(COURS);
 
         mockMvc.perform(post("/cours")
                                 .content(json)
@@ -86,13 +97,22 @@ class CoursControllerTest {
     }
 
     @Test
-    @Description("Test unitaire du controller pour vérifier le statut de"
-                 + " la requête de création d'un cours avec un identifiant non nul")
-    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Test unitaire controller pour créer un cours avec un identifiant non nul ou invalide")
+    @Description("Envoi d'une requête pour créer un cours avec un identifiant non nul ou invalide."
+                 + " On s'attend à un statut 400.")
+    @Severity(SeverityLevel.NORMAL)
     void createCoursTestKo() throws Exception {
 
-        cours.setIdCours(1);
-        final String json = new ObjectMapper().writeValueAsString(cours);
+        COURS.setIdCours(1);
+        String json = new ObjectMapper().writeValueAsString(COURS);
+
+        mockMvc.perform(post("/cours")
+                                .content(json)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        COURS.setDureeCours((byte) 10);
+        json = new ObjectMapper().writeValueAsString(COURS);
 
         mockMvc.perform(post("/cours")
                                 .content(json)
@@ -101,12 +121,13 @@ class CoursControllerTest {
     }
 
     @Test
-    @Description("Test unitaire du controller pour vérifier le statut de"
-                 + " la requête de modification d'un cours")
+    @DisplayName("Test unitaire controller pour modifier un cours")
+    @Description("Envoi d'une requête pour modifier le cours un identifiant 1."
+                 + " On s'attend à un statut 400 car aucun cours dans la BDD.")
     @Severity(SeverityLevel.NORMAL)
     void updateCoursTest() throws Exception {
 
-        final String json = new ObjectMapper().writeValueAsString(cours);
+        final String json = new ObjectMapper().writeValueAsString(COURS);
 
         mockMvc.perform(put("/cours/1")
                                 .content(json)
@@ -115,12 +136,13 @@ class CoursControllerTest {
     }
 
     @Test
-    @Description("Test unitaire du controller pour vérifier le statut de"
-                 + " la requête de suppression d'un cours")
+    @DisplayName("Test unitaire controller pour supprimer un cours")
+    @Description("Envoi d'une requête pour supprimer le cours un identifiant 1."
+                 + " On s'attend à un statut 400 car aucun cours dans la BDD.")
     @Severity(SeverityLevel.NORMAL)
     void deleteCoursTest() throws Exception {
 
-        final String json = new ObjectMapper().writeValueAsString(cours);
+        final String json = new ObjectMapper().writeValueAsString(COURS);
 
         mockMvc.perform(delete("/cours/1")
                                 .content(json)
